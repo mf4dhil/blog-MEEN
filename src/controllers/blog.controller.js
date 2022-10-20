5be077659000a7c695122d100000b5c53eaa7c94
Muhammad Fadhil <syafwah999@gmail.com>
2022-10-20 16:10:42 +0700

    Menghapus data berdasarkan id

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -1,62 +1,68 @@
-const Article = require('../models/article.model')
+const Article = require("../models/article.model");
 
 exports.create = (req, res) => {
-    res.render('blog/create')
-}
+  res.render("blog/create");
+};
 
 exports.store = async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        subtitle: req.body.subtitle,
-        content: req.body.content,
-    })
+  let article = new Article({
+    title: req.body.title,
+    subtitle: req.body.subtitle,
+    content: req.body.content,
+  });
 
-    try {
-        await article.save()
-        res.redirect('/')
-    } catch (e) {
-        res.redirect('blog/create')
-    }
-}
+  try {
+    await article.save();
+    res.redirect("/");
+  } catch (e) {
+    res.redirect("blog/create");
+  }
+};
 
-exports.show = async (req,res) => {
-    try {
-        const id = req.params.id
-        let article = await Article.findById(id)
-        res.render('blog/show', {
-            article: article,
-        })
-    } catch (e) {
-        res.redirect('/')
-    }
-}
+exports.show = async (req, res) => {
+  try {
+    const id = req.params.id;
+    let article = await Article.findById(id);
+    res.render("blog/show", {
+      article: article,
+    });
+  } catch (e) {
+    res.redirect("/");
+  }
+};
 
-exports.edit = async (req,res) => {
-    try {
-        const id = req.params.id
-        let article = await Article.findById(id)
-        res.render('blog/edit', {
-            article: article,
-        })
-    } catch (e) {
-        res.redirect('/')
-    }
-}
+exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    let article = await Article.findById(id);
+    res.render("blog/edit", {
+      article: article,
+    });
+  } catch (e) {
+    res.redirect("/");
+  }
+};
 
-exports.update = async (req,res) => {
-    try {
-        const id = req.params.id
-        let article = await Article.findById(id)
-        article.title = req.body.title
-        article.subtitle = req.body.subtitle
-        article.content = req.body.content
+exports.update = async (req, res) => {
+  try {
+    const id = req.params.id;
+    let article = await Article.findById(id);
+    article.title = req.body.title;
+    article.subtitle = req.body.subtitle;
+    article.content = req.body.content;
 
-        await article.save()
+    await article.save();
 
-        res.redirect('/')
-    } catch (e) {
-        res.render('blog/edit', {
-            article: article,
-        })
-    }
-}
+    res.redirect("/");
+  } catch (e) {
+    res.render("blog/edit", {
+      article: article,
+    });
+  }
+};
+
+exports.delete = async (req, res) => {
+  const id = req.params.id;
+  await Article.findByIdAndDelete(id);
+  res.redirect("/");
+};
