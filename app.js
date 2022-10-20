096697b316acec896fe4e36f88964080b6ed3f18
Muhammad Fadhil <syafwah999@gmail.com>
2022-10-20 09:51:00 +0700

    templating dengan ejs view

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = 5000
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
 require('./src/routes/home.routes')(app)
 require('./src/routes/about.routes')(app)
 
