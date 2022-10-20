4870a9088cd64a251db044ca3bc85b0ce8140218
Muhammad Fadhil <syafwah999@gmail.com>
2022-10-20 10:58:51 +0700

    mendapatkan data dari form dangan form-urlencoded

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const PORT = 5000
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({
+    extended:false
+}))
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
@@ -13,7 +16,6 @@ app.use('/img', express.static(__dirname + 'public/img'))
 
 require('./src/routes/home.routes')(app)
 require('./src/routes/blog.routes')(app)
-require('./src/routes/about.routes')(app)
 
 app.listen(PORT, () => {
     console.log(`Starting development server: http://127.0.0.1:${PORT}`)
