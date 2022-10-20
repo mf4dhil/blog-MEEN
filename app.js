52a7c6d9681f79e16a2bdd3393f16e6443b968ed
Muhammad Fadhil <syafwah999@gmail.com>
2022-10-20 11:27:14 +0700

    konfigurasi database dan fungsi save article

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const  express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 const PORT = 5000
+
+mongoose.connect('mongodb://localhost:27017/lear_blog_with_meen')
 
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
